f40df661a738cc2b9aa463e910a7c13266848d87
Aaron Friel <mayreply@aaronfriel.com>
2022-07-20 10:28:36 -0700

    More configuration options, control interval and 'always log' interval

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,7 +1,8 @@
 use std::{collections::HashMap, fmt::Write, path::Path, time::Duration};
+use std::{collections::HashMap, fmt::Write, path::Path, time::Duration, str::FromStr};
 
 use structopt::{paw, StructOpt};
-use sysinfo::{Pid, Process, ProcessExt, System, SystemExt};
+use sysinfo::{set_open_files_limit, Pid, Process, ProcessExt, System, SystemExt};
 use tokio::{
   fs::File,
   io::{stderr, AsyncWriteExt},
@@ -39,7 +40,7 @@ pub struct ProgramArgs {
   out: Option<String>,
 
   #[structopt(flatten)]
-  threshold_options: ThresholdOptions,
+  options: Options,
 
   /// Program to run
   #[structopt()]
@@ -51,7 +52,7 @@ pub struct ProgramArgs {
 }
 
 #[derive(StructOpt, Debug, Clone)]
-struct ThresholdOptions {
+struct Options {
   /// The minimum increase, in kilobytes, over the high water mark required
   /// to output stats.
   #[structopt(short = "a", long, default_value = "1024")]
@@ -61,11 +62,25 @@ struct ThresholdOptions {
   /// to output stats.
   #[structopt(short = "r", long, default_value = "0")]
   threshold_relative: f64,
+
+  /// How frequently, in milliseconds, to check memory stats.
+  #[structopt(short = "i", long, default_value = "250")]
+  check_interval: u64,
+
+  /// The minimum number of intervals to wait between reporting memory stats
+  /// without reaching a high water mark.
+  #[structopt(short = "n", long)]
+  report_every_nth: Option<u8>,
+
+  #[structopt(short = "c", long)]
+  print_cmd: bool,
 }
 
 #[paw::main]
 #[tokio::main]
 async fn main(args: ProgramArgs) -> Result<(), Box<dyn std::error::Error>> {
+  set_open_files_limit(0);
+
   let mut spawned_process = tokio::process::Command::new(args.program)
     .args(args.args)
     .kill_on_drop(true)
@@ -79,12 +94,7 @@ async fn main(args: ProgramArgs) -> Result<(), Box<dyn std::error::Error>> {
     .expect("Expected process to have a valid pid") as PidT;
 
   let output_file = get_file(args.out.as_deref()).await?;
-  let handle = tokio::task::spawn(measure_memory(
-    pid,
-    child_token,
-    output_file,
-    args.threshold_options,
-  ));
+  let handle = tokio::task::spawn(measure_memory(pid, child_token, output_file, args.options));
 
   spawned_process.wait().await?;
   token.cancel();
@@ -114,7 +124,7 @@ async fn measure_memory(
   pid: PidT,
   child_token: CancellationToken,
   output_file: Option<File>,
-  threshold_options: ThresholdOptions,
+  threshold_options: Options,
 ) {
   measure_memory_internal(pid, child_token, output_file, threshold_options)
     .await
@@ -125,9 +135,10 @@ async fn measure_memory_internal(
   pid: PidT,
   child_token: CancellationToken,
   mut output_file: Option<File>,
-  threshold_options: ThresholdOptions,
+  options: Options,
 ) -> Result<(), Box<dyn std::error::Error>> {
-  let mut timer = time::interval(Duration::from_millis(100));
+  let mut timer = time::interval(Duration::from_millis(options.check_interval));
+  let mut i = 0;
   let mut sys = System::new_all();
   let pid = (pid as PidT).into();
   let mut high_water_mark_kib: u64 = 0;
@@ -135,31 +146,49 @@ async fn measure_memory_internal(
   loop {
     sys.refresh_processes();
     let processes = sys.processes();
-
     let process_children = get_process_children(processes);
     let process_memory = compute_memory_stats(pid, &process_children);
 
     if let Some(stats) = process_memory.get(&pid) {
       let aggregate_kib = stats.aggregate_kib;
-      let met_threshold_absolute =
-        aggregate_kib > high_water_mark_kib + threshold_options.threshold_absolute;
-      let met_threshold_relative = aggregate_kib
-        > ((high_water_mark_kib as f64) * (1.0 + threshold_options.threshold_relative)) as u64;
-      if met_threshold_absolute && met_threshold_relative {
-        eprintln!(
-          "🌊 gotta-watch-em-all: Reached a new high water mark of {} KiB, {} greater than before!",
-          aggregate_kib,
-          aggregate_kib - high_water_mark_kib
-        );
-        high_water_mark_kib = aggregate_kib;
+      let met_threshold_absolute = aggregate_kib > high_water_mark_kib + options.threshold_absolute;
+      let met_threshold_relative =
+        aggregate_kib > ((high_water_mark_kib as f64) * (1.0 + options.threshold_relative)) as u64;
+      let met_thresholds = met_threshold_absolute && met_threshold_relative;
 
-        if let Err(err) =
-          print_stats(pid, &process_children, &process_memory, &mut output_file).await
-        {
-          eprintln!("Error: {:?}", err)
+      let print_anyway = match options.report_every_nth {
+        Some(n) if n > 0 => i % n == 0,
+        _ => false,
+      };
+      if met_thresholds || print_anyway {
+        if met_thresholds {
+          eprintln!(
+            "🌊 gotta-watch-em-all: high water mark reached: {} MiB",
+            aggregate_kib / 1024
+          );
+          high_water_mark_kib = aggregate_kib;
+        } else {
+          eprintln!(
+            "🌊 gotta-watch-em-all: presently at {} MiB",
+            aggregate_kib / 1024,
+          );
         }
+
+        match print_stats(
+          pid,
+          &process_children,
+          &process_memory,
+          &mut output_file,
+          &options,
+        )
+        .await
+        {
+          Err(err) => eprintln!("Error: {:?}", err),
+          _ => {}
+        };
       }
     };
+    i = i + 1;
 
     select! {
         _ = child_token.cancelled() => {
@@ -183,24 +212,32 @@ async fn print_stats<'a>(
   process_children: &HashMap<Pid, ProcessEntry<'a>>,
   process_memory: &HashMap<Pid, MemoryStats>,
   output: &mut Option<File>,
+  options: &Options,
 ) -> Result<(), Box<dyn std::error::Error>> {
   let mut buffer = String::new();
 
   let title = "process";
-  let private_kib = "private ";
-  let aggregate_kib = "total ";
+  let private_mib = "private ";
+  let aggregate_mib = "total ";
   let depth = 0;
   writeln!(
     buffer,
-    "🌊 {SPACE:<indent$}{title:<28}{SPACE:<width$}{private_kib:>9}KiB {aggregate_kib:>9}KiB",
+    "🌊 {SPACE:<indent$}{title:<50}{SPACE:<width$}{private_mib:>9}MiB {aggregate_mib:>9}MiB",
     indent = depth * 2,
-    width = 30 - depth * 2,
+    width = 30 - (depth * 2),
   )?;
 
   // We've reached a high water mark, print useful info. First we'll get all the
   // processes and their individual and aggregate memories:
   if let Some(entry) = process_children.get(&pid) {
-    record_high_water_mark_entry(&mut buffer, entry, process_children, process_memory, 0)?;
+    record_high_water_mark_entry(
+      &mut buffer,
+      entry,
+      process_children,
+      process_memory,
+      0,
+      &options,
+    )?;
   }
   writeln!(buffer)?;
 
@@ -218,29 +255,57 @@ fn record_high_water_mark_entry(
   process_children: &HashMap<Pid, ProcessEntry>,
   process_memory: &HashMap<Pid, MemoryStats>,
   depth: usize,
+  options: &Options,
 ) -> Result<(), Box<dyn std::error::Error>> {
   let process = entry.process.unwrap();
   let process_exe = process.exe();
   let name = Path::new(process_exe)
     .file_name()
-    .and_then(|x| x.to_str())
-    .unwrap_or_else(|| process.name());
+    .map(|x| x.to_str())
+    .flatten()
+    .unwrap_or(process.name());
   let pid = process.pid();
   let title = format!("{name} ({pid})");
   let MemoryStats {
-    private_kib,
+    private_mib,
     aggregate_kib,
   } = process_memory
     .get(&process.pid())
     .copied()
     .unwrap_or_default();
+  let aggregate_mib = aggregate_kib / 1024;
 
   writeln!(
     buffer,
-    "🌊 {SPACE:<indent$}{title:<28}{SPACE:<width$}{private_kib:>9}KiB {aggregate_kib:>9}KiB",
+    "🌊 {SPACE:<indent$}{title:<50}{SPACE:<width$}{private_mib:>9}MiB {aggregate_mib:>9}MiB",
     indent = depth * 2,
     width = 30 - depth * 2,
   )?;
+
+  if options.print_cmd {
+    let mut line_buffer = String::from_str("  ")?;
+    let cmd = process.cmd();
+
+    let mut clear_buffer = |line_buffer: &mut String, min_length: usize| -> Result<(), Box<dyn std::error::Error>> {
+      if line_buffer.len() > min_length {
+        writeln!(
+          buffer,
+          "🌊 {SPACE:<indent$}{line_buffer}",
+          indent = depth * 2,
+        )?;
+        line_buffer.clear();
+        write!(line_buffer, "    ")?;
+      }
+
+      Ok(())
+    };
+
+    for arg in cmd {
+      clear_buffer(&mut line_buffer, 100)?;
+      write!(line_buffer, " {arg}")?;
+    }
+    clear_buffer(&mut line_buffer, 0)?;
+  }
 
   for child_pid in entry.children.iter() {
     if let Some(child_entry) = process_children.get(child_pid) {
@@ -250,6 +315,7 @@ fn record_high_water_mark_entry(
         process_children,
         process_memory,
         depth + 1,
+        options,
       )?;
     }
   }
@@ -275,7 +341,7 @@ fn get_process_children(processes: &HashMap<Pid, Process>) -> HashMap<Pid, Proce
 #[derive(Copy, Clone, Default)]
 pub struct MemoryStats {
   aggregate_kib: u64,
-  private_kib: u64,
+  private_mib: u64,
 }
 
 fn compute_memory_stats(
@@ -297,8 +363,9 @@ fn compute_aggregate_memory_kib(
 ) -> u64 {
   let process = entry.process.unwrap();
 
-  let private_kib = process.memory();
-  let mut aggregate_kib: u64 = private_kib;
+  let memory = process.memory();
+  let private_mib = memory / 1024;
+  let mut aggregate_kib: u64 = memory;
 
   for child_pid in entry.children.iter() {
     if let Some(child_entry) = process_children.get(child_pid) {
@@ -310,7 +377,7 @@ fn compute_aggregate_memory_kib(
     process.pid(),
     MemoryStats {
       aggregate_kib,
-      private_kib,
+      private_mib,
     },
   );
 
